488a5a0da88fb539069b4b1fdf6a84b9693573df
“Shubham” <shubhampattar2@gmail.com>
2020-06-09 20:51:17 +0530

    UC3 Completed

--- snake_ladder_problem.sh
+++ snake_ladder_problem.sh
@@ -3,9 +3,33 @@
 echo "Welcome to Snake-Ladder Simulation";
 POS=0;
 
+
 play()
 {
 	local DICE=$(((RANDOM%6)+1));
-	echo $DICE;
+	echo "Dice : "$DICE;
+	local OPTION=$(((RANDOM%3)+1));
+	case $OPTION in
+		1 )
+			echo "Option : No Play";
+			echo "Current Position "$POS;
+			;;
+		2 )
+			echo "Option : Ladder";
+			POS=$((POS+DICE));
+			echo "Current Position "$POS;
+			;;
+		3 )
+			echo "Option : Snake";
+			if [[ $POS -eq 0 ]]; 
+			then
+				POS=$POS;
+			else
+				POS=$((POS-DICE));
+			fi
+			echo "Current Position "$POS;
+			;;
+	esac
 }
+
 play
